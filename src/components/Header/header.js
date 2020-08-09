189d28a9002afa317331c9cf7aa3e4bc96b47838
shpaksasha <shpaksasha0000@gmail.com>
2020-08-09 15:58:36 +0300

    header.js changes block Navbar

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -3,11 +3,20 @@ import {makeStyles} from '@material-ui/core/styles';
 import {Navbar, Container, Nav, Form, FormControl, Button, NavDropdown}from 'react-bootstrap';
 import logo from '../../images/calendar_3.jpg'
 import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
+import FilterIcon from '@material-ui/icons/Filter';
+import DynamicFeedIcon from '@material-ui/icons/DynamicFeed';
+import ShowChartIcon from '@material-ui/icons/ShowChart';
 
 const useStyles = makeStyles(theme => ({
-    addCircle: {
-        color: 'blue',
-        
+    addIcon: {
+        color: '#4791db',
+        paddingRight: '7px'
+    },
+    block:{
+        color: 'red',
+        '&:hover':{
+            backgroundColor: 'yellow'
+        }
     }
 }));
 
@@ -27,14 +36,14 @@ const Header = () =>{
                     Calendar
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls='responsive-navbar-nav'/>
-                <Navbar.Collapse id='responsive-navbar-nav'>
+                <Navbar.Collapse id='responsive-navbar-nav'className={classes.block}>
                     <Nav className='m-auto pr-md-2'>
                         <NavDropdown title='Графік' id='basic-nav-dropdown'>
-                            <NavDropdown.Item href=''><AddCircleOutlineIcon className={classes.addCircle}/>Створити графік</NavDropdown.Item>
-                            <NavDropdown.Item href=''>Порівняти графіки</NavDropdown.Item>
-                            <NavDropdown.Item href=''>Всі графіки</NavDropdown.Item>
+                            <NavDropdown.Item href=''><AddCircleOutlineIcon className={classes.addIcon}/>Створити графік</NavDropdown.Item>
+                            <NavDropdown.Item href=''><FilterIcon className={classes.addIcon}/>Порівняти графіки</NavDropdown.Item>
+                            <NavDropdown.Item href=''><DynamicFeedIcon className={classes.addIcon}/>Всі графіки</NavDropdown.Item>
                             <NavDropdown.Divider/>
-                            <NavDropdown.Item href=''>Статистика</NavDropdown.Item>
+                            <NavDropdown.Item href=''><ShowChartIcon className={classes.addIcon}/>Статистика</NavDropdown.Item>
                         </NavDropdown>
                         <Nav.Link href='/content'>Запросити друзів</Nav.Link>
                         <Nav.Link href='/faq'>Підтримка</Nav.Link>
