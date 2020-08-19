60afaf5d72fbfec99dd1b03ed48cd3f8e3a9401e
shpaksasha <shpaksasha0000@gmail.com>
2020-08-19 23:12:01 +0300

    works on Contact Form

--- src/components/LogOn/log.js
+++ src/components/LogOn/log.js
@@ -5,9 +5,24 @@ import {makeStyles} from '@material-ui/core/styles';
 const useStyles = makeStyles(theme => ({
     form: {
         width: '400px',
-        height: '400px',
+        height: '480px',
         margin: '180px auto',
-        position: 'relative'
+        position: 'relative',
+        backgroundColor:'#f5f5f5',
+        borderRadius: '10px',
+        padding: '2em 2em'
+    },
+    control: {
+        paddingBottom:'7px',
+    },
+    contact: {
+        fontSize: '1.9em',
+        fontFamily: 'Roboto',
+        position: 'relative',
+        padding: '1.7em 2em'
+    },
+    submit: {
+        margin: '0em auto'
     }
 }));
 
@@ -15,22 +30,20 @@ const LogOn = () => {
     const classes = useStyles();
     return (
         <Form className={classes.form}>
-            <Form.Group controlId='formName'>
-                <Form.Label>Name</Form.Label>
-                <Form.Control type='name' placeholder='Name'/>
+            <Form.Label className={classes.contact}>Contact Form</Form.Label>
+            <Form.Group controlId='formName' className={classes.control}>
+                <Form.Control style={{fontFamily: 'Roboto'}} type='name' placeholder='Name'/>
             </Form.Group>
 
-            <Form.Group controlId='formEmail'>
-                <Form.Label>Email</Form.Label>
-                <Form.Control type='email' placeholder='Email'/>
+            <Form.Group controlId='formEmail' className={classes.control}>
+                <Form.Control style={{fontFamily: 'Roboto'}} type='email' placeholder='Email'/>
             </Form.Group>
 
             <Form.Group controlId='Message'>
-                <Form.Label>Message</Form.Label>
-                <Form.Control as='textarea' type='message' placeholder='Message'/>
+                <Form.Control style={{fontFamily: 'Roboto'}} as='textarea' type='message' placeholder='Message'/>
             </Form.Group>
 
-            <Button variant="primary" type='submit'>
+            <Button className={classes.submit} class="btn btn-dark" variant='dark' type='submit'>
                 Submit
             </Button>
         </Form>
