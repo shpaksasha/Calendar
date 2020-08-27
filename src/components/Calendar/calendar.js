7069c941197be487546178545557bea705e8a177
shpaksasha <shpaksasha0000@gmail.com>
2020-08-27 23:16:14 +0300

    class component change to huck useState

--- src/components/Calendar/calendar.js
+++ src/components/Calendar/calendar.js
@@ -2,21 +2,20 @@ import React, {useState} from 'react';
 import Calendar from 'react-calendar';
 
 const CalendarReact = () => {
-    // state = {
-    //     date: new Date(),
-    // }
-    // onChange = date => this.setState({ date })
-    const [date, setDate] = useState([new Date()]);
 
-    
-        return (
-            <div>
-                <Calendar
-                    onChange={this.onChange}
-                    value={this.state.date}
-                />
-            </div>
-        );
+    const [date, setDate] = useState(new Date());
+
+    const onChange = date => {
+        setDate(date);
+    }
+
+    return (
+        <div>
+            <Calendar
+                onChange={onChange}
+                value={date}/>
+        </div>
+    );
 
 }
 
