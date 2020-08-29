ea509ba52c4030be0597e6ce875e2bf4687828ed
shpaksasha <shpaksasha0000@gmail.com>
2020-08-29 15:21:48 +0300

    component Calendar, add styles

--- src/components/Calendar/calendar.js
+++ src/components/Calendar/calendar.js
@@ -1,7 +1,18 @@
 import React, {useState} from 'react';
 import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import {makeStyles} from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+    form:{
+        position: 'relative',
+        left: '50%',
+        transform: 'translate(-50%, 0)'
+    }
+}))
 
 const CalendarReact = () => {
+    const classes = useStyles();
 
     const [date, setDate] = useState(new Date());
 
@@ -12,11 +23,13 @@ const CalendarReact = () => {
     return (
         <div>
             <Calendar
+                className={classes.form}
+                defaultView='mouth'
+                calendarType='ISO 8601'
                 onChange={onChange}
                 value={date}/>
         </div>
     );
-
 }
 
 export default CalendarReact;
