2abf860f28cce94406c2dae334a29686ac767229
shpaksasha <shpaksasha0000@gmail.com>
2020-09-15 23:03:10 +0300

    new simple card page, componenta

--- src/components/LogOn/log.js
+++ src/components/LogOn/log.js
@@ -33,17 +33,17 @@ const useStyles = makeStyles(theme => ({
     // root: {
     //     minWidth: 275,
     // },
-    bullet: {
-        display: 'inline-block',
-        margin: '0 2px',
-        transform: 'scale(0.8)',
-    },
-    title: {
-        fontSize: 14,
-    },
-    pos: {
-        marginBottom: 12,
-    },
+    // bullet: {
+    //     display: 'inline-block',
+    //     margin: '0 2px',
+    //     transform: 'scale(0.8)',
+    // },
+    // title: {
+    //     fontSize: 14,
+    // },
+    // pos: {
+    //     marginBottom: 12,
+    // },
 }));
 
 const LogOn = () => {
@@ -86,36 +86,36 @@ const LogOn = () => {
     //     });
     // }
 
-    const SimpleCard = () => {
-        let baseRef = db.collection('emails').get()
-            .then(snapshot => {
-                snapshot.map((item) => {
-                    return (
-                        <Card className={classes.root}>
-                            <CardContent>
-                                <Typography className={classes.title} color="textSecondary" gutterBottom>
-                                    Database card information
-                                </Typography>
-                                <Typography variant="h5" component="h2">
-                                   `${item.name}: ${item.email}: ${item.message}`
-                                </Typography>
-                                <Typography className={classes.pos} color="textSecondary">
-                                    adjective
-                                </Typography>
-                                <Typography variant="body2" component="p">
-                                    well meaning and kindly.
-                                    <br />
-                                    {'"a benevolent smile"'}
-                                </Typography>
-                            </CardContent>
-                            <CardActions>
-                                <Button size="small">Learn More</Button>
-                            </CardActions>
-                        </Card>
-                    )
-                })
-            })
-    }
+    // const SimpleCard = () => {
+    //     let baseRef = db.collection('emails').get()
+    //         .then(snapshot => {
+    //             snapshot.map((item) => {
+    //                 return (
+    //                     <Card className={classes.root}>
+    //                         <CardContent>
+    //                             <Typography className={classes.title} color="textSecondary" gutterBottom>
+    //                                 Database card information
+    //                             </Typography>
+    //                             <Typography variant="h5" component="h2">
+    //                                `${item.name}: ${item.email}: ${item.message}`
+    //                             </Typography>
+    //                             <Typography className={classes.pos} color="textSecondary">
+    //                                 adjective
+    //                             </Typography>
+    //                             <Typography variant="body2" component="p">
+    //                                 well meaning and kindly.
+    //                                 <br />
+    //                                 {'"a benevolent smile"'}
+    //                             </Typography>
+    //                         </CardContent>
+    //                         <CardActions>
+    //                             <Button size="small">Learn More</Button>
+    //                         </CardActions>
+    //                     </Card>
+    //                 )
+    //             })
+    //         })
+    // }
 
 
     return (
