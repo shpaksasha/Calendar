f21126ca87f97db92eaecff6fb58f906cd4a7b89
shpaksasha <shpaksasha0000@gmail.com>
2021-04-19 22:53:34 +0300

    add changes to project

--- src/index.js
+++ src/index.js
@@ -6,8 +6,6 @@ import {BrowserRouter} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-      <BrowserRouter>
-    <App/>
-      </BrowserRouter>
-  </React.StrictMode>, document.getElementById('root'));
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>, document.getElementById('root'));
